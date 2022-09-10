325c17df66ecd8bebfa8b202a55e2a4e2eb54b5c
hshunter98 <106415285+hshunter98@users.noreply.github.com>
2022-09-09 21:49:59 -0700

    Add files via upload

--- Part2.java
+++ Part2.java
@@ -1,6 +1,25 @@
 
 public class Part2 {
 
+	public static void main(String[] args) {
+		Array a = new Array(10);
+		a.setVal(0, 1);
+		a.setVal(1, 2);
+		a.setVal(2, 3);
+		a.setVal(3, 4);
+		a.setVal(4, 5);
+		a.setVal(5, 6);
+		a.setVal(6, 7);
+		a.setVal(7, 0);
+		a.setVal(8, 0);
+		a.setVal(9, 0);
+		int x = a.getAccessCount();
+		System.out.println("Array before = "+a.toString());
+		pushZeroes(a);
+		System.out.println("Array after  = "+a.toString());
+		System.out.println(a.getAccessCount()-x);
+	}
+	
 	public static void pushZeroes(Array a) {
 		int x;
 		int back = a.length()-1;
